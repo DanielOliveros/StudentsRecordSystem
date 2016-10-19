4736cdd1df90053fa36cab117938c58b4f495f75
Mohan9346 <xumohan9346@gmail.com>
2016-10-20 04:21:23 +0800

    Add classes to Domain.

---
+++ StudentsRecordSystem/src/Domain/Lecturer.java
@@ -0,0 +1,38 @@
+package Domain;
+
+
+public class Lecturer {
+	private int id;
+	private String name;
+	private String password;
+	
+	public Lecturer(int id,String name,String password){
+		this.id = id;
+		this.name = name;
+		this.password = password;		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
