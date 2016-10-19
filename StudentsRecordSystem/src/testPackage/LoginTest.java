18d7095c6aca2cfa86a88fbed71f5a6e2a7ee5c1
DanielOliveros <daniel92oliveros@gmail.com>
2016-10-19 21:15:17 +0200

    First test sample

---
+++ StudentsRecordSystem/src/testPackage/LoginTest.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
