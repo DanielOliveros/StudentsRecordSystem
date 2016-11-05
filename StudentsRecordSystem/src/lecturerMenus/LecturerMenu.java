9d977045cfd28404fa71f9ebf281a45e3697948b
DanielOliveros <daniel92oliveros@gmail.com>
2016-11-05 18:17:33 +0000

    LecturerMenu

--- StudentsRecordSystem/src/lecturerMenus/LecturerMenu.java
+++ StudentsRecordSystem/src/lecturerMenus/LecturerMenu.java
@@ -18,5 +18,20 @@ public class LecturerMenu implements Menu{
 
 	public void display() {
 		System.out.println("LECTURER MENU");
+		System.out.println("Please select the module to manage");
+		//List<Module> modules = ManageAllocatedModules.getCurrentUserModules()
+		//for(int i=0; i<modules.size(); i++){ print module details}
+		System.out.println("Insert the id of one of the modules above");
+		//int id=scan.nextInt();
+		//managemodule(id);
+	}
+	public void manageModule(String id){
+		System.out.prinln("Module management: ...");
+		Module module = ManageModule.getModule(id);
+		if(module.ready==false){
+			setAssessmentModel(module);
+		}else{
+			assessModule(module); //QPV subject and QCA observer.
+		}
 	}
 }
